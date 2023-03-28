e46cabe7e7bd65689bc09764697641e762629b48
Noa <76524924+Noa-Nussbaum@users.noreply.github.com>
2023-03-28 19:02:16 +0000

    Added tests

--- Test.cpp
+++ Test.cpp
@@ -3,12 +3,15 @@
 #include "sources/game.hpp"
 #include "sources/player.hpp"
 #include <stdexcept>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 
 using namespace std;
 
 namespace ariel{
 
 TEST_CASE("Player initialization"){
+
     Player player1("Noa");
     Player player2("Hodaya");
     Game game(player1,player2);
@@ -18,12 +21,16 @@ TEST_CASE("Player initialization"){
 
     CHECK(player2.stacksize()==26);
     CHECK(player2.cardesTaken()==0);
+
 }
 
 TEST_CASE("Game initialization"){
+
     Player player1("Noa");
     Player player2("Hodaya");
+
     Game Game(player1,player2);
+
 
     REQUIRE_NOTHROW(Game.playTurn());
     REQUIRE_NOTHROW(Game.printLastTurn());
@@ -35,9 +42,12 @@ TEST_CASE("Game initialization"){
 }
 
 TEST_CASE("Game has started"){
+
     Player player1("Noa");
     Player player2("Hodaya");
+
     Game Game(player1,player2);
+
     Game.playTurn();
     CHECK(player1.stacksize()==25);
     CHECK(player2.stacksize()==25);
@@ -45,7 +55,66 @@ TEST_CASE("Game has started"){
 }
 
 TEST_CASE("Entire game"){
+
+    Player player1("Noa");
+    Player player2("Hodaya");
+
+    Game Game(player1,player2);
+    for(int i=0; i<7;i++){
+        Game.playTurn();
+    }
+
+    CHECK(player1.stacksize()<=19);
+    CHECK(player2.stacksize()<=19);
+
+    Game.playAll();
+
+    CHECK(player1.stacksize()==0);
+    CHECK(player2.stacksize()==0);
     
+}
+
+TEST_CASE("Check winners"){
+
+    Player player1("Noa");
+    Player player2("Hodaya");
+
+    Game Game(player1,player2);
+
+    Game.playAll();
+
+    // check throws for play turn after play all
+    CHECK_THROWS(Game.playTurn());
+
+     // check if print equals the winner
+    int one = player1.cardesTaken();
+    int two = player2.cardesTaken();
+
+    // Save the current standard output stream to a variable
+    std::stringstream ss;
+    std::streambuf* old_cout_buf = std::cout.rdbuf();
+    std::cout.rdbuf(ss.rdbuf());
+
+    // Call the function that prints to standard output
+    Game.printWiner();
+
+    // Restore the original standard output stream
+    std::cout.rdbuf(old_cout_buf);
+
+    if(one>two){
+        // Check that the output is correct
+        std::string output = ss.str();
+        CHECK(output == "Noa\n");
+    }
+    else{
+        // Check that the output is correct
+    std::string output = ss.str();
+    CHECK(output == "Hodaya\n");
+    }
+}
+
+TEST_CASE(""){
+
 }
 
 
